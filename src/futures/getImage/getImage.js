be506ba55434b786e3776b35fe818b862cbffeb1
s1ma82 <s1mahochukvas@vk.com>
2023-09-01 21:23:18 +0300

    favicon google api doesn't workining with vercel host

--- src/futures/getImage/getImage.js
+++ src/futures/getImage/getImage.js
@@ -1,5 +1,7 @@
 export default function (data) {
     const { url } = data
+    console.log(new URL(url))
+
     const domain = new URL(url).hostname
     return `https://www.google.com/s2/favicons?domain=${domain}&sz=128`
 }
