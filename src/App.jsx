bfa5cd3cd88a98659438d2c680d8c171807dd88f
simon <s1mahochukvas@vk.com>
2024-02-01 22:12:28 +0300

    new step to auto update

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,40 @@
 import React, { useEffect, useState } from 'react'
 import { Provider } from 'react-redux'
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import {Analytics} from '@vercel/analytics/react'
 import { CustomStyles } from '@components'
 
 import {store} from './redux/store'
+import { setExtension } from './redux/extension/extension.slice'
 import Main from './pages/Main.page'
 
-import { setExtension } from './redux/extension/extension.slice'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const isExtension = () => {
 	const [proto] = window.origin.split(":")
 	store.dispatch(setExtension(proto === 'chrome-extension'))
 }
 
-window.EXTENSION_FILE_LINK = "https://github.com/s1ma82/lvplvce/releases"
+window.EXTENSION_FILE_LINK = "https://github.com/s1ma82/lvplvce/releases/latest/download/"
 
 const ModalWindow = React.lazy(() => import("./components/ModalWindow/ModalWindow.tsx"))
 const ContextMenu = React.lazy(() => import("./components/ContextMenu/ContextMenu.tsx"))
+
+const checkForUpdates = async () => {
+  	const response = await fetch('https://api.github.com/repos/s1ma82/lvplvce/releases/latest');
+  	const data = await response.json();
+	console.log(data)	
+  	const latestVersion = data.tag_name;
+  	const currentVersion = chrome.runtime.getManifest().version;
+
+  	if (latestVersion !== currentVersion) {
+  	  // Есть новая версия, выполните необходимые действия для обновления
+  	  chrome.runtime.reload();
+  	}
+};	
+
+checkForUpdates();
 function App() {
 	useEffect(() => {
-		
 		(async () => {
 			await import('./styles/global.css')
 		})().then(() => {
@@ -33,7 +48,8 @@ function App() {
 			<CustomStyles/>
 			<Main />
 			<ModalWindow/>
-			<ContextMenu/>
+			<ContextMenu />
+			<Analytics/>
 		</Provider>
 	)
 }
